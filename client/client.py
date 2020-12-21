96f48a92ee9ffbd631a5fcc35b15c72f9a040c93
Anton-7 <antmat@ttu.ee>
2020-12-21 15:07:25 +0200

    Issue3: prepared architecture for conversation with members.

--- client/client.py
+++ client/client.py
@@ -8,20 +8,48 @@ from db.database import Database
 class BotClient(discord.Client):
     """Bot client."""
 
+    def __init__(self, command_prefix, intents):
+        """Constructor."""
+        super().__init__(command_prefix=command_prefix, intents=intents)
+        self.db = Database()
+
     async def on_ready(self):
-        db = Database()
-        print("Bot is ready")
+        print("===DEBUG: Bot is ready")
 
     async def on_member_join(self, member):
-        print(f"{member} has joined the server!")
-        await member.send("Hey!")
+        print(f"===DEBUG: {member} has joined the server!")
+        await member.send("Hey! Enter your school e-mail. I'll send you a confirmation code.")
+        self.db.add_new_member(member)
 
     async def on_member_remove(self, member):
-        print(f"{member} has left the server!")
+        print(f"===DEBUG: {member} has left the server!")
 
     async def on_message(self, message):
         if message.author == self.user:
             return
         else:  # Private messages.
             if type(message.channel) is discord.channel.DMChannel:
-                print(message.content)
+                print(f"===DEBUG: Message object: {message}")
+                print(f"===DEBUG: Message content: {message.content}")
+                print(f"===DEBUG: Message author: {message.author}")
+                if self.db.is_user_in_table(message.author):
+                    if self.db.is_code_sent(message.author):
+                        if self.db.get_code(message.author) == message.content:
+                            await message.author.send("You look like a student. Welcome aboard.")
+                            self.accept_user()
+                    if not self.db.update_mail(message.author, message.content):
+                        await message.author.send("Your e-mail looks incorrect. Try again.")
+                    else:
+                        code = self.generate_code()
+                        self.db.update_code(message.author, code)
+                        await message.author.send("I've sent you a code. Enter it.")
+                        print(f"===DEBUG: Generated code: {code}")
+
+    def generate_code(self):
+        """Generate verification code."""
+        # TODO
+        return "123"
+
+    def accept_user(self):
+        """Accepting user, changing the role etc."""
+        # TODO
--- db/database.py
+++ db/database.py
@@ -6,9 +6,14 @@ class Database:
 
     def __init__(self):
         """Database constructor."""
-        db = sqlite3.connect("main.sqlite")
-        cursor = db.cursor()
-        cursor.execute("""
+        self.db = sqlite3.connect("main.sqlite")
+        self.cursor = self.db.cursor()
+        self._is_code_sent = False
+        self.init_table()
+
+    def init_table(self):
+        """Create table if necessary."""
+        self.cursor.execute("""
                     CREATE TABLE IF NOT EXISTS members(
                     user_id TEXT,
                     user_mail TEXT,
@@ -17,15 +22,50 @@ class Database:
                     )
                 """)
 
-    def add_new_member(self):
+    def add_new_member(self, member):
         """Add a new member to the database using the discord ID as user_id
         and leave mail and code empty for now."""
         # TODO
+        print(f"===DEBUG: {member}")
 
     def update_mail(self, user, mail):
         """Update user's mail."""
         # TODO
+        print(f"===DEBUG: setting an email {mail} for {user}")
+        if self.is_correct_mail(mail):
+            pass
+        else:
+            pass
+        return True
 
     def update_code(self, user, code):
         """Update user's code for authentication."""
         # TODO
+        print(f"===DEBUG: setting a code {code} for {user}")
+        self.send_code_to_mail()
+
+    def is_user_in_table(self, user):
+        """Check if the user is already in our table."""
+        # TODO
+        print(f"===DEBUG: checking if an user {user} is in table")
+        return True
+
+    def is_correct_mail(self, mail):
+        """Check if entered e-mail's domain is correct."""
+        # TODO
+        print(f"===DEBUG: checking if an email {mail} is correct")
+        return True
+
+    def is_code_sent(self, user):
+        """Make sure the code is sent to user. And change the corresponding value in the table."""
+        # TODO
+        return self._is_code_sent
+
+    def send_code_to_mail(self):
+        """Send an e-mail with the code."""
+        self._is_code_sent = True
+
+    def get_code(self, user):
+        """Return generated user's code from the table."""
+        # TODO
+        return "123"
