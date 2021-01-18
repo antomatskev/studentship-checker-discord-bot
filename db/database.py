99df0f4015de3a18f643e53bfd09b636bb1e2589
Anton-7 <antmat@ttu.ee>
2021-01-18 19:48:08 +0200

    Issue14: fixed returns to be shorter and formatted codestyle a bit.

--- db/database.py
+++ db/database.py
@@ -25,7 +25,7 @@ class Database:
                     is_code_sent INTEGER
                     )
                 """)
-        self.db.commit()  # Is it need to be here??
+        self.db.commit()
 
     def add_new_member(self, member: discord.User.id):
         """Add a new member to the database using the discord ID as user_id
@@ -37,21 +37,20 @@ class Database:
         self.db.commit()
         print(f"===DEBUG: {member}")
         self.cursor.execute("""SELECT * FROM members""")
-        print(self.cursor.fetchone())
+        print(f"===DEBUG: {self.cursor.fetchone()}")
 
     def update_mail(self, user, mail):
         """Update user's mail."""
         print(f"===DEBUG: setting an email {mail} for {user}")
-        if self.is_correct_mail(mail):
+        ret = self.is_correct_mail(mail)
+        if ret:
             self.cursor.execute(f"""
                 UPDATE members
                 SET user_mail = '{mail}'
                 WHERE user_id = '{user}'
             """)
             self.db.commit()
-        else:
-            return False
-        return True
+        return ret
 
     def update_code(self, user, code):
         """Update user's code for authentication."""
@@ -78,42 +77,38 @@ class Database:
             SELECT * FROM members WHERE EXISTS 
             (SELECT user_id FROM members WHERE user_id = '{user}')
         """)
-        res = self.cursor.fetchone()
-        return res
+        return self.cursor.fetchone()
 
     def is_correct_mail(self, mail: str):
         """Check if entered e-mail's domain is correct."""
         print(f"===DEBUG: checking if an email {mail} is correct")
-        res = False
-        try:
-            res = True if len(mail.split("@")[0]) > 0 \
-                          and (mail.split("@")[1] == 'ttu.ee' or mail.split("@")[1] == 'taltech.ee') else False
-        finally:
-            pass
-        return res
+        # TODO: i've seen an exception here (try-except didn't help). Will need to test more or switch to "contains".
+        return len(mail.split("@")[0]) > 0 and (mail.split("@")[1] == 'ttu.ee' or mail.split("@")[1] == 'taltech.ee')
 
     def code_was_sent(self, user):
         """Updating is_code_sent field in database to 1"""
         self.cursor.execute(f"""
-            UPDATE members SET is_code_sent = 1 WHERE user_id = '{user}'
-        """)
+                UPDATE members SET is_code_sent = 1 WHERE user_id = '{user}'
+            """)
         self.db.commit()
 
     def is_code_sent(self, user):
         """Make sure the code is sent to user. And change the corresponding value in the table."""
         self.cursor.execute(f"""
-            SELECT is_code_sent FROM members WHERE user_id = '{user}'
-        """)
-        res = True if self.cursor.fetchone()[0] == 1 else False
-        return res
+                SELECT is_code_sent FROM members WHERE user_id = '{user}'
+            """)
+        return self.cursor.fetchone()[0]
 
     def send_code_to_mail(self, usermail, code):
         """Send an e-mail with the code."""
         receiver = usermail
-        body = f"""Привет! Судя по всему, Вы пытаетесь стать полноправным учасником сервера TalTech-IT-rus. Чтобы получить роль и право писать в чате, пришлите код ниже боту, которому Вы дали свою электронную почту:
-        {code}
-        Не добавляйте в сообщение для бота никаких дополнительных знаков и слов, и пришлите код единым сообщением, не разделяя его.
-        Если вы не запрашивали подобных писем, просто не обращайте внимания это сообщение."""
+        # TODO: the text should be in English and more generic and shorter. And we should ask the client to choose it.
+        body = f"""Привет! Судя по всему, Вы пытаетесь стать полноправным учасником сервера TalTech-IT-rus. \
+        Чтобы получить роль и право писать в чате, пришлите код ниже боту, которому Вы дали свою электронную почту:
+            {code}
+            Не добавляйте в сообщение для бота никаких дополнительных знаков и слов, и пришлите код единым сообщением, \
+            не разделяя его.
+            Если вы не запрашивали подобных писем, просто не обращайте внимания это сообщение."""
 
         # Only gmail account can be used. Need to provide user(example -> something@gmail.com) and password.
         yag = yagmail.SMTP(user="", password="")
@@ -127,7 +122,6 @@ class Database:
     def get_code(self, user):
         """Return generated user's code from the table."""
         self.cursor.execute(f"""
-            SELECT user_code FROM members WHERE user_id = '{user}'
-        """)
-        res = self.cursor.fetchone()[0]
-        return res
+                SELECT user_code FROM members WHERE user_id = '{user}'
+            """)
+        return self.cursor.fetchone()[0]
